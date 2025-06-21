7d3bc3277537471a7cdd90f41ab67cc539b590b4
anis2720 <138358919+anis2720@users.noreply.github.com>
2025-06-22 01:28:31 +0800

    Update publicPath for new repo name

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,10 +7,11 @@
         I'm <strong>Nurul Anis</strong>, a fresh graduate in Game Technology from Universiti Teknikal Malaysia Melaka (UTeM) with a strong passion for programming and game development. I have hands-on experience in developing interactive digital projects, including custom tools for environmental data monitoring and visualization, which reflect my ability to blend creativity with technical skill.
         </div>
 
-        <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">game projects</router-link>, on <router-link to="/other-projects">other projects</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
+        <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">game projects</router-link>, on <router-link to="/other-projects">other projects</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.
         </div>
-        
+
       <div style="margin-top: 40px;">I am eager to apply my knowledge and enthusiasm to real-world projects, particularly in game design, software development, and emerging technologies. Dedicated to continuous learning and professional growth, I am excited to contribute to the tech and digital innovation industry through meaningful, impactful work.
+      </div>
     </div>
 
     <div class="photo">
